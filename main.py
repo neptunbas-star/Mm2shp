2921c18e4d8018d100bf713b941783475c6fd5f1
neptunbas-star <neptunbas@gmail.com>
2026-06-29 16:44:41 +0500

    main.py

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import user, admin
-from aiohttp import web
 from database import init_db
 
-
-async def handle(request): return web.Response(text="Бот в сети")
+# Настройка логирования (чтобы видеть ошибки в консоли)
+logging.basicConfig(level=logging.INFO)
 
 async def main():
-    # Запуск веб-сервера для Render
-    app = web.Application()
-    app.add_routes([web.get('/', handle)])
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 8080)
-    await site.start()
-    await init_db()
-
+    # Инициализация бота и диспетчера
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-    dp.include_router(user.router)
-    dp.include_router(admin.router) # Добавь эту строку
 
+    # Запуск базы данных
+    await init_db()
+
+    # Подключение роутеров (частей бота)
+    dp.include_router(user.router)
+    dp.include_router(admin.router)
+
+    # Запуск бота
+    print("Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот выключен")
