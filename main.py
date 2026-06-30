6ee29f6b9fb7b3f312cefe3615c2adc129538b76
neptunbas-star <neptunbas@gmail.com>
2026-06-30 19:28:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import create_db
 from handlers.start import router as start_router
+from handlers.admin import router as admin_router
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start_router)
+dp.include_router(shop_router)
+dp.include_router(admin_router)
 
 
 async def main():
