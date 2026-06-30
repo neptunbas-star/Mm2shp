9063d217224aa391e5cba84ed7cdf0c624b0ccdb
neptunbas-star <neptunbas@gmail.com>
2026-06-30 20:16:57 +0500

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,9 +1,17 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from database import add_product
 from config import ADMIN_ID
 from keyboards.admin import admin_menu
 
 router = Router()
+
+class AddProduct(StatesGroup):
+    category = State()
+    name = State()
+    price = State()
 
 @router.message(lambda m: m.text == "/admin")
 async def admin_panel(message: Message):
