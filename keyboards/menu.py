20cd0e5e689fef6751f5f3e25318b2216538cdb6
neptunbas-star <neptunbas@gmail.com>
2026-06-30 19:19:21 +0500

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,38 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🛒 MM2 Прайс")],
+        [KeyboardButton(text="👑 Админ Прайс")],
+        [KeyboardButton(text="📢 Пиар Прайс")],
+        [KeyboardButton(text="⭐ Отзывы")],
+    ],
+    resize_keyboard=True
+)
+
+buy_button = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="💳 Купить",
+                callback_data="buy"
+            )
+        ]
+    ]
+)
+
+reviews_button = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="⭐ Открыть отзывы",
+                url="https://t.me/rishaproofsss"
+            )
+        ]
+    ]
+)
