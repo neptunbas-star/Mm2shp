572086664b5c697241a549dfda164ae8bdb8221d
neptunbas-star <neptunbas@gmail.com>
2026-06-30 19:32:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from config import BOT_TOKEN
 from database import create_db
 from handlers.start import router as start_router
 from handlers.admin import router as admin_router
+from handlers.checks import router as checks_router
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
@@ -13,6 +14,7 @@ dp = Dispatcher()
 dp.include_router(start_router)
 dp.include_router(shop_router)
 dp.include_router(admin_router)
+dp.include_router(checks_router)
 
 
 async def main():
