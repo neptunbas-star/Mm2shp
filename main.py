ab32e0f6e0e91f4a4993f7e5837fdb93934b1e96
neptunbas-star <neptunbas@gmail.com>
2026-06-30 19:22:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from aiogram import Bot, Dispatcher
 import asyncio
 
-# Импортируем токен из твоего файла config.py
-from config import BOT_TOKEN
+from aiogram import Bot, Dispatcher
 
-# Инициализируем бота и диспетчер
-bot = Bot(token=BOT_TOKEN)
+from config import BOT_TOKEN
+from database import create_db
+from handlers.start import router as start_router
+
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-# Здесь позже будем подключать роутеры (handlers)
-# dp.include_router(user.router)
+dp.include_router(start_router)
+
 
 async def main():
-    print("Бот успешно запущен!")
-    # Запуск процесса опроса (polling)
+    await create_db()
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
